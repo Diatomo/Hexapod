209e96b849d3c121b5c2bc0f050ec2215e3cf760
Diatomo <Wojtekenson@gmail.com>
2019-06-19 15:09:20 -0400

    hexapod changes

--- master/components/hexapod.py
+++ master/components/hexapod.py
@@ -6,10 +6,74 @@ from utils import *
 import time
 from leg import Leg
 
+
+'''
+    class Legs
+
+        collection class being utilized as a data class
+        resembling a struct
+'''
+class Legs:
+
+    def __init__(self):
+        
+        '''
+            Legs
+        '''
+        self.leg1 = Leg(1, Vector3(-61.167, 24, 98), 300)
+        self.leg2 = Leg(2, Vector3(61.167, 24, 98), 60)
+        self.leg3 = Let(3, Vector3(81, 24, 0), 0)
+        self.leg4 = Leg(4, Vector3(61.167, 24, -98), 120)
+        self.leg5 = Leg(5, Vector3(-61.167, 24, -98), 240)
+        self.leg6 = Leg(6, Vector3(-81, 24, 0), 270)
+        
+        #leg collection
+        self.legs = { 
+                "FL" : self.leg1,  
+                "FR" : self.leg2, 
+                "MR" : self.leg3, 
+                "BR" : self.leg4, 
+                "BL" : self.leg5, 
+                "ML" : self.leg6
+        }
+
+        '''
+            Feets
+        '''
+        self.foot1 = self.homeFootPosition(Vector3(), leg1, Pose())
+        self.foot2 = self.homeFootPosition(Vector3(), leg2, Pose())
+        self.foot3 = self.homeFootPosition(Vector3(), leg3, Pose())
+        self.foot4 = self.homeFootPosition(Vector3(), leg4, Pose())
+        self.foot5 = self.homeFootPosition(Vector3(), leg5, Pose())
+        self.foot6 = self.homeFootPosition(Vector3(), leg6, Pose())
+        
+        #feet collection
+        self.feet = {
+                "FL" : self.foot1, 
+                "FR" : self.foot2, 
+                "MR" : self.foot3,
+                "BR" : self.foot4,
+                "BL" : self.foot5,
+                "ML" : self.foot6
+        }
+
+        '''
+            Poses 
+        '''
+        self.target = Pose()
+        self.lastPose = Pose()
+        self.lastFeet = []
+        self.nextFeet = []
+
+'''
+    class Hexapod
+
+        a State machine to route action to the legs
+'''
 class Hexapod:
 
     def __init__(self):
-
+        #states
         state = ""
         stateCounter = 0
 
@@ -24,8 +88,7 @@ class Hexapod:
 	# The distance (in mm) to adjust the Y position to meet the Y target each
 	# tick. This mostly controls the time it takes to stand up and sit down.
         self.yMoveSpeed = 1
-
-        self.bankMoveSpeed = 0.5
+        self.yawMoveSpeed = 0.5
         self.pitchMoveSpeed = 0.5
 
 	# Distance (on the X/Z axis) from the origin to the point at which the feet
@@ -56,40 +119,8 @@ class Hexapod:
 	# be determined experimentally; too high and the legs get tangled up.
         self.maxStepDistance = 90.0
 
-        self.leg1 = Leg(1, Vector3(-61.167, 24, 98), 300)
-        self.leg2 = Leg(2, Vector3(61.167, 24, 98), 60)
-        self.leg3 = Let(3, Vector3(81, 24, 0), 0)
-        self.leg4 = Leg(4, Vector3(61.167, 24, -98), 120)
-        self.leg5 = Leg(5, Vector3(-61.167, 24, -98), 240)
-        self.leg6 = Leg(6, Vector3(-81, 24, 0), 270)
-        
-        self.legs = { 
-                "FL" : self.leg1,  
-                "FR" : self.leg2, 
-                "MR" : self.leg3, 
-                "BR" : self.leg4, 
-                "BL" : self.leg5, 
-                "ML" : self.leg6
-        }
-
-        self.foot1 = self.homeFootPosition(Vector3(), leg1, Pose())
-        self.foot2 = self.homeFootPosition(Vector3(), leg2, Pose())
-        self.foot3 = self.homeFootPosition(Vector3(), leg3, Pose())
-        self.foot4 = self.homeFootPosition(Vector3(), leg4, Pose())
-        self.foot5 = self.homeFootPosition(Vector3(), leg5, Pose())
-        self.foot6 = self.homeFootPosition(Vector3(), leg6, Pose())
-        
-        self.feet = {
-                "FL" : self.foot1, 
-                "FR" : self.foot2, 
-                "MR" : self.foot3,
-                "BR" : self.foot4,
-                "BL" : self.foot5,
-                "ML" : self.foot6
-        }
-
-        self.lastFeet = []
-        self.nextFeet = []
+        #leg && feet collection
+        self.legs = Legs()
 
     def start(self):
 
@@ -107,6 +138,18 @@ class Hexapod:
         v.y = 0.0
         return v
 
+    def distanceFromHome(self):
+        td = 0.0
+
+        for key, leg in self.legs:
+            pv = leg.presentPosition()
+            td += pv.distance(self.legs.feet[key]) #TODO check index syntax
+
+    '''
+        boot functions
+            sets moving speed
+            sets torque limit
+    '''
     def bootSlow(self):
         for (key, leg in self.legs):
             for (servo in leg):
@@ -120,20 +163,25 @@ class Hexapod:
                 servo.setMovingSpeed(self.moveSpeedFast)
                 servo.setTorqueLimit(self.torqueLimitFast)
 
+    '''
+        States for State Machine
+    '''
     def default(self):
         self.bootFast()
         self.state = "standup"
+
+    def standup(self):
+
 
     def tick(self, cTime):
         if (self.state == "default"):
             self.default()
         elif (self.state == "standup"):
-            pass#TODO
+            self.standup()
         elif (self.state == "sitDown"):
             pass#TODO
         elif (self.state == "stepping"):
             pass#TODO
-
 
     def waitForReady(self):
         pass
@@ -145,7 +193,11 @@ class Hexapod:
             return maximum
         return v
 
-
+    def setState(self, s):
+        self.stateCounter = 0
+        self.stateTime = time.time()
+        self.state = s
+    
     def makeGait(self):
         pass
 
