afedd43b073c29b150137280f3c571c240f321d4
Diatomo <Wojtekenson@gmail.com>
2019-07-18 18:15:06 -0400

    pose updates

--- master/components/pose.py
+++ master/components/pose.py
@@ -5,22 +5,66 @@ path.append("../utils")
 from utils import *
 
 
+
+
+#TODO
+'''
+    Description of pose here
+'''
 class Pose:
 
+    '''
+        constructor
+            param: ea = euler angles
+            @param: v = vector3
+    '''
     def __init__(self, ea, v):
         self.angles = ea
         self.pos = v
 
+    '''
+        fxn: add
+            adds two poses together
+    '''
     def add(self, pose):
         pose.multiplyByMatrix44(self.pos.toWorld())
         self.angles.roll = self.angles.roll + pose.angles.roll
         self.angles.pitch = self.angles.pitch + pose.angles.pitch
         self.angles.yaw = self.angles.yaw + pose.angles.yaw
 
+    '''
+        fxn: toWorld
+            converts pose into its world coordinates
+    '''
     def toWorld(self):
         return makeMatrix(self.pos, self.angles)
-
-    #TODO perhaps inverse this?
+    
+    '''
+        fxn :: toLocal
+            turns pose into its local coordinates
+    '''
     def toLocal(self):
         return makeMatrix(self.pos, self.angles)
+
+    
+    #TODO
+    def out(self, pp):
+        pass
+
+    #TODO
+    def eulerAngles(self):
+        pass
+
+
+    '''
+        fxn :: toString
+            outputs pose in string format
+    '''
+    def toString(self):
+        print("position x: " + self.pos.x)
+        print("position y: " + self.pos.y)
+        print("position z: " + self.pos.z)
+        print("euler roll: " + self.angles.roll)
+        print("euler pitch: " + self.angles.pitch)
+        print("euler yaw: " + self.angles.yaw)
 
